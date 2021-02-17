b306bdc38ff34b0bffac1563326f9c3b861e1b5c
leafs <70290483+ayleafs@users.noreply.github.com>
2021-02-16 23:26:09 -0500

    Update PotionEffectHandler.java

--- src/main/java/com/thekingelessar/miningfatiguealert/PotionEffectHandler.java
+++ src/main/java/com/thekingelessar/miningfatiguealert/PotionEffectHandler.java
@@ -13,40 +13,33 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class PotionEffectHandler
-{
-    
-    public static boolean hasMiningFatigue = false;
+public class PotionEffectHandler {
+    private static boolean hasMiningFatigue = false;
     
     @SideOnly (Side.CLIENT)
     @SubscribeEvent (priority = EventPriority.NORMAL, receiveCanceled = true)
-    public void onEvent(TickEvent.ClientTickEvent tickEvent)
-    {
+    public void onEvent(TickEvent.ClientTickEvent tickEvent) {
         Minecraft minecraft = Minecraft.getMinecraft();
         EntityPlayerSP player = minecraft.thePlayer;
         
-        if (player == null)
-        {
+        if (player == null) {
             return;
         }
         
         PotionEffect potionEffect = player.getActivePotionEffect(Potion.digSlowdown);
         
-        if (potionEffect != null)
-        {
-            if (hasMiningFatigue)
-            {
+        if (potionEffect != null) {
+            if (hasMiningFatigue) {
                 return;
             }
             
             player.playSound("minecraft:mob.creeper.death", 1, 1);
             player.addChatMessage(new ChatComponentText("You have received the " + ChatFormatting.GOLD + ChatFormatting.BOLD + "mining fatigue " + ChatFormatting.RESET + "effect!"));
             hasMiningFatigue = true;
+            return;
         }
-        else
-        {
-            if (hasMiningFatigue) hasMiningFatigue = false;
-        }
+
+        hasMiningFatigue = false;
     }
     
 }
